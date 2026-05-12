703dff91a8cbb262f36b4ed145e93a356ab75730
AshinaGenichiro <uT_4011@zsp3zamosc.pl>
2026-05-12 13:20:06 +0200

    rozdział 3 zad 4

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import Header from './components/header.jsx'
 import DateTime from './components/DateTimeCard.jsx'
-
+import Analiza from './components/Analiza.jsx'
+import Code from './components/CodeSnippet.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header/>
-    <DateTime/>
+    <Header/> 
+    <DateTime/> 
+    <Analiza/>
+    <Code title='Witaj' language='JS' code='const element = ( <> <h1>Tytuł</h1> <p>Opis</p> </> );'/>
     <App />
   </StrictMode>,
 )

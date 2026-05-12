86d8672053219e5d6bc316c59b69ab1944deb548
AshinaGenichiro <uT_4011@zsp3zamosc.pl>
2026-05-12 11:55:24 +0200

    rozdziały 1-3, w rozdziele 3 do zad 3

---
+++ src/components/header.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from '../assets/react.svg'
+import viteLogo from '../assets/vite.svg'
+import heroImg from '../assets/hero.png'
+import '../App.css'
+
+function Header() {
+  return (
+    <>
+      <nav>
+        <ul style={{display:"block",listStyleType: "none"} }>
+            <li><a href="#">jakis</a></li>
+            <li><a href="#">link</a></li>
+            <li><a href="#">rar</a></li>
+            <li><a href="#">100</a></li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default Header
